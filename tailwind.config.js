2583abfa61556b7e3ce534fbb4b8c8cbe4b618f6
David Gutiérrez <dvdgp93@gmail.com>
2026-04-10 13:23:03 +0200

    Aligerar la página

---
+++ tailwind.config.js
@@ -0,0 +1,72 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./politica-privacidad.html",
+    "./politica-cookies.html",
+    "./condiciones-servicio.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "on-tertiary-container": "#bcf3ff",
+        "on-surface-variant": "#3f484d",
+        "on-secondary-fixed-variant": "#1b4c61",
+        "primary": "#00586f",
+        "on-tertiary": "#ffffff",
+        "surface-container-lowest": "#ffffff",
+        "on-background": "#171c1e",
+        "inverse-primary": "#80d1f2",
+        "secondary-fixed-dim": "#9fcde5",
+        "on-secondary-fixed": "#001e2b",
+        "surface-variant": "#dee3e5",
+        "surface-container-low": "#eff4f6",
+        "secondary": "#366479",
+        "on-secondary-container": "#39667c",
+        "surface-container": "#eaeff1",
+        "tertiary-fixed": "#a1efff",
+        "secondary-fixed": "#c0e8ff",
+        "inverse-on-surface": "#ecf2f4",
+        "surface": "#f5fafc",
+        "on-tertiary-fixed": "#001f25",
+        "on-secondary": "#ffffff",
+        "on-primary-fixed": "#001f29",
+        "surface-container-highest": "#dee3e5",
+        "on-error": "#ffffff",
+        "surface-tint": "#006781",
+        "tertiary": "#005a66",
+        "outline-variant": "#bec8cd",
+        "on-surface": "#171c1e",
+        "on-error-container": "#93000a",
+        "primary-fixed": "#baeaff",
+        "tertiary-container": "#007483",
+        "error-container": "#ffdad6",
+        "surface-container-high": "#e4e9eb",
+        "on-primary-container": "#cbefff",
+        "outline": "#6f787d",
+        "error": "#ba1a1a",
+        "surface-bright": "#f5fafc",
+        "background": "#f5fafc",
+        "inverse-surface": "#2c3133",
+        "secondary-container": "#b5e3fd",
+        "on-tertiary-fixed-variant": "#004e59",
+        "on-primary": "#ffffff",
+        "tertiary-fixed-dim": "#44d8f1",
+        "surface-dim": "#d6dbdd",
+        "on-primary-fixed-variant": "#004d62",
+        "primary-container": "#00728f",
+        "primary-fixed-dim": "#80d1f2"
+      },
+      borderRadius: {
+        DEFAULT: "0.125rem",
+        lg: "0.25rem",
+        xl: "0.5rem",
+        full: "0.75rem"
+      },
+      fontFamily: {
+        headline: ["Lexend", "sans-serif"],
+        body: ["Manrope", "sans-serif"],
+        label: ["Lexend", "sans-serif"]
+      }
+    }
+  }
+};
